832a9e8c3508e648f325659ad2af01025b713ee8
yonghun16 <yonghun16@naver.com>
2026-07-11 23:05:31 +0900

    Update autocmds.lua

--- config/nvim/lua/custom/autocmds.lua
+++ config/nvim/lua/custom/autocmds.lua
@@ -2,18 +2,25 @@ require "nvchad.autocmds"
 
 local autocmd = vim.api.nvim_create_autocmd
 
+-- 상대번호 활성화 여부
+local function update_relativenumber()
+  local bt = vim.bo.buftype
+  local modifiable = vim.bo.modifiable
+
+  -- 일반 파일이 아니면 끔
+  if bt ~= "" or not modifiable then
+    vim.wo.relativenumber = false
+  else
+    vim.wo.relativenumber = true
+  end
+end
+
 -- CursorLine: 포커스 창에서만 활성화
 autocmd({ "WinEnter", "BufEnter" }, {
   callback = function()
     vim.o.cursorlineopt = "both"
     vim.o.cursorline = true
-
-    -- 터미널에서는 상대번호를 켜지 않음
-    if vim.bo.buftype ~= "terminal" then
-      vim.wo.relativenumber = true
-    else
-      vim.wo.relativenumber = false
-    end
+    update_relativenumber()
   end,
 })
 
@@ -25,16 +32,17 @@ autocmd({ "WinLeave", "BufLeave" }, {
   end,
 })
 
--- relativenumber : Neovim이 비활성화되면 상대번호 끄기
+-- Neovim이 비활성화되면 상대번호 끄기
 autocmd("FocusLost", {
   callback = function()
     vim.wo.relativenumber = false
   end,
 })
 
+-- Neovim으로 다시 돌아오면 조건에 따라 상대번호 켜기
 autocmd("FocusGained", {
   callback = function()
-    vim.wo.relativenumber = true
+    update_relativenumber()
   end,
 })
 
