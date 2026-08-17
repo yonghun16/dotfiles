666a4bd07d26aba38b9f1a5ee706e2a09b39d342
yonghun16 <yonghun16@naver.com>
2026-08-17 16:02:15 +0900

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,7 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = ","
+vim.g.clipboard = "osc52"
+vim.opt.clipboard = "unnamedplus"
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
