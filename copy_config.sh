79a5dcea6b0984290040e987de92c64a641012d6
yonghun16 <yonghun16@naver.com>
2026-09-25 17:40:55 +0900

    copy_config

---
+++ copy_config.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+\cp -rf ~/.config/nvim/lua/custom ~/dotfiles/config/nvim/lua/
+\cp -rf ~/.config/nvim/templates ~/dotfiles/config/nvim/
+
+\cp -rf ~/.config/karabiner/karabiner.json ~/dotfiles/config/karabiner/
+
+\cp -rf ~/.config/ghostty/config ~/dotfiles/config/ghostty/
+
+\cp -rf ~/.termux/* ~/dotfiles/termux/
+
+\cp -rf ~/.tmux.conf ~/dotfiles/tmux.conf
