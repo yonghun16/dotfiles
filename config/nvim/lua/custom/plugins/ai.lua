379a21d05441671d83b4c95f5a4f210f591bed48
yonghun16 <yonghun16@naver.com>
2026-09-26 21:49:15 +0900

    Update ai.lua

--- config/nvim/lua/custom/plugins/ai.lua
+++ config/nvim/lua/custom/plugins/ai.lua
@@ -96,12 +96,6 @@ local plugins = {
         chat = {
           adapter = "ollama",
 
-          tools = {
-            opts = {
-              default_tools = { "insert_edit_into_file", "read_file" },
-            },
-          },
-
           keymaps = {
             close = {
               modes = {
